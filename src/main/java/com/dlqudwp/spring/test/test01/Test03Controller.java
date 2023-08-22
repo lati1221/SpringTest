0d87e372f4fd48e3754567b1b7e8ba7455fab926
dlqudwp <dlqudwp21@naver.com>
2023-08-22 17:57:42 +0900

    [이병제] 8.22 수업 (1시간남음)

---
+++ src/main/java/com/dlqudwp/spring/test/test01/Test03Controller.java
@@ -0,0 +1,15 @@
+package com.dlqudwp.spring.test.test01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Test03Controller {
+	
+	@RequestMapping("/test/test03")
+	public String test03View() {
+		return "test/test03";
+		
+	}
+
+}
