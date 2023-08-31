9408033cfd6d3787c2b331d8d69e21ecc0fca55d
dlqudwp <dlqudwp21@naver.com>
2023-08-31 18:48:39 +0900

    [이병제] 8.31 수업

--- src/main/java/com/dlqudwp/spring/test/jstl/JSTLController.java
+++ src/main/java/com/dlqudwp/spring/test/jstl/JSTLController.java
@@ -1,14 +1,71 @@
 package com.dlqudwp.spring.test.jstl;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class JSTLController {
 	
-	@GetMapping("jstl/test01")
+	@GetMapping("/jstl/test01")
 	public String test01() {
 		return "/jstl/test01";
 	}
+	
+	@GetMapping("/jstl/test03")
+	public String test03(Model model) {
+		
+		List<Integer> candidates = new ArrayList<>();
+		candidates.add(263001);
+		candidates.add(173942); 
+		candidates.add(563057); 
+		
+		model.addAttribute("candidates", candidates);
+		
+		return "/jstl/test03";
 
+	}
+	
+	@GetMapping("/jstl/test04")
+	public String test04(Model model) {
+		
+		List<Map<String, Object>> cardBills = new ArrayList<>();
+
+		Map<String, Object> cardBill = new HashMap<>();
+		cardBill.put("store", "GS48");
+		cardBill.put("pay", 7800);
+		cardBill.put("date", "2025-09-14");
+		cardBill.put("installment", "일시불");
+		cardBills.add(cardBill);
+
+		cardBill = new HashMap<>();
+		cardBill.put("store", "현태백화점");
+		cardBill.put("pay", 2750000);
+		cardBill.put("date", "2025-09-18");
+		cardBill.put("installment", "3개월");
+		cardBills.add(cardBill);
+
+		cardBill = new HashMap<>();
+		cardBill.put("store", "요촌치킨");
+		cardBill.put("pay", 180000);
+		cardBill.put("date", "2025-09-20");
+		cardBill.put("installment", "일시불");
+		cardBills.add(cardBill);
+		
+		model.addAttribute("card", cardBills);
+		
+		
+		
+		return "/jstl/test04";
+		
+		
+		
+	}
+	
+	
 }
---
+++ src/main/java/com/dlqudwp/spring/test/jstl/JSTLTest2Controller.java
@@ -0,0 +1,33 @@
+package com.dlqudwp.spring.test.jstl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class JSTLTest2Controller {
+	
+	@GetMapping("/jstl/test02")
+	public String test02(Model model) {
+		
+		List<String> musicRanking = new ArrayList<>();
+		musicRanking.add("강남스타일");
+		musicRanking.add("벚꽃엔딩");
+		musicRanking.add("좋은날");
+		musicRanking.add("거짓말");
+		musicRanking.add("보고싶다");
+		
+		model.addAttribute("musicRanking", musicRanking);
+		
+		
+		return "jstl/test02";
+		
+	}
+	
+	
+	
+	
+}
