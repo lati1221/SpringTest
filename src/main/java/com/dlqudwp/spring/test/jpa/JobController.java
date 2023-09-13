35a4003677803f21cc76b2dc6fdaa66cefd31650
dlqudwp <dlqudwp21@naver.com>
2023-09-13 18:50:03 +0900

    [이병제] 0913 수업

--- src/main/java/com/dlqudwp/spring/test/jpa/JobController.java
+++ src/main/java/com/dlqudwp/spring/test/jpa/JobController.java
@@ -1,15 +1,19 @@
 package com.dlqudwp.spring.test.jpa;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.dlqudwp.spring.test.jpa.domain.Job;
 import com.dlqudwp.spring.test.jpa.repository.JobRepository;
 
+@RequestMapping("/jpa/job/select")
 @Controller
 public class JobController {
 	
@@ -17,16 +21,71 @@ public class JobController {
 	private JobRepository jobRepository;
 	
 	
-	@GetMapping("/jpa/job/select")
+	@GetMapping("/1")
 	@ResponseBody
-	public List<Job> selectJob() {
+	public Job selectJob() {
 		
-		List<Job> jobList = jobRepository.findById(8);
+		Optional<Job> optionalJob = jobRepository.findById(8);
+		Job job = optionalJob.orElse(null);
 		
-		
-		return jobList;
+		return job;
 		
 	}
+	
+	
+	@GetMapping("/2")
+	@ResponseBody
+	public List<Job> selectJob2(@RequestParam("companyId") int companyId) {
+		List<Job> jobList = jobRepository.findByCompanyId(companyId);
+		
+		return jobList;
+	}
+	
+	@GetMapping("/3")
+	@ResponseBody
+	public List<Job> selectJob3() {
+		
+		List<Job> jobList = jobRepository.findByPositionAndType("웹 back-end 개발자", "정규직");
+		
+		return jobList;
+	}
+	
+	@GetMapping("/4")
+	@ResponseBody
+	public List<Job> selectJob4() {
+		List<Job> jobList = jobRepository.findByTypeOrSalaryGreaterThanEqual("정규직", 9000);
+		
+		return jobList;
+	}
+	
+	@GetMapping("/5")
+	@ResponseBody
+	
+	public List<Job> selectJob5() {
+		
+	  List<Job> jobList = jobRepository.findTop3ByTypeOrderBySalaryDesc("계약직");
+		
+	  return jobList;
+	  
+	}
+	
+	@GetMapping("/6")
+	@ResponseBody
+	public List<Job> selectJob6() {
+		
+		return jobRepository.findByRegionAndSalaryBetween("성남시 분당구", 7000, 8500);
+		
+	}
+	
+	@GetMapping("/7")
+	@ResponseBody
+	public List<Job> selectJob7() {
+		
+		return jobRepository.findByNativeQuery("2026-04-10", 8100, "정규직");
+		
+		
+	}
+	
 	
 
 }
--- src/main/java/com/dlqudwp/spring/test/jpa/repository/JobRepository.java
+++ src/main/java/com/dlqudwp/spring/test/jpa/repository/JobRepository.java
@@ -3,6 +3,8 @@ package com.dlqudwp.spring.test.jpa.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.dlqudwp.spring.test.jpa.domain.Job;
@@ -10,7 +12,39 @@ import com.dlqudwp.spring.test.jpa.domain.Job;
 @Repository
 public interface JobRepository extends JpaRepository<Job, Integer> {
 
-	public List<Job> findById(int id);
+	// companyId 컬럼이 일치하는 행 조회
+	// WHERE `companyId` = ??
+	public List<Job> findByCompanyId(int companyId);
+	
+	// position 컬럼과 type컬럼이 모두 일치하는 행 조회
+	// WHERE `position` = ?? AND `type` = ??
+	public List<Job> findByPositionAndType(String position, String type);
+	
+	// type 컬럼이 일치하거나 salary 컬럼의 값이 특정 값보다 이상인 행 조회
+	// WHERE `type` = ?? OR `salary` >= ???
+	public List<Job> findByTypeOrSalaryGreaterThanEqual(String type, int salary);
+	
+	
+	// type 컬럼이 일치하는 행을 연봉기준으로 내림차순으로 3개 조회
+	// WHERE `type` = ?? ORDER BY `salary` DESC LIMIT 3
+	public List<Job> findTop3ByTypeOrderBySalaryDesc(String type);
+	
+	// region 컬럼이 일치하고 salary 컬럼이 특정값 사이의 조건에 대응되는 행 조회
+	// WHERE `region` = ?? AND `salary` BETWEEN ?? AND ??
+	public List<Job> findByRegionAndSalaryBetween(String region, int salaryStart, int salaryEnd);
+	
+	// deadline 컬럼이 특정 값보다 크고 salary가 특정 값 보다 크고 type이 일치하는 데이터를 연봉 내림차순으로 조회 
+	@Query(value="SELECT * FROM `job` "
+			+ "WHERE `deadline` > :deadline "
+			+ "AND `salary` >= :salary "
+			+ "AND `type` = :type "
+			+ "ORDER BY `salary` DESC", nativeQuery=true)
+	public List<Job> findByNativeQuery(
+			@Param("deadline") String deadline
+			, @Param("salary") int salary
+			, @Param("type") String type);
+	
+	
 	
 	
 }
